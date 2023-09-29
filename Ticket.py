857640b7ec86887533c767cb2b2b695666eeb9c9
Andrew Schweizer <Andrewschweizer01@gmail.com>
2023-09-28 23:52:58 -0400

    First version - opens browser and closes Need to add interactions Need to add checkout interactions Need to add time loop

--- Ticket.py
+++ Ticket.py
@@ -1,30 +1,45 @@
 from datetime import datetime
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options  # Import ChromeOptions
 
-#URL of the checkout page
+# URL of the StubHub checkout page
 checkout_url = 'https://www.stubhub.com/p-nk-arlington-tickets-9-29-2023/event/151219653/?quantity=2'
 
-#start purchase time example (10:00:00 AM)
-purchase_time = datetime.now().replace(hour=8, minute=20, second=0, microsecond=0)
+# Start purchase time (adjust this to your desired purchase time)
+purchase_time = datetime.now().replace(hour=10, minute=0, second=0, microsecond=0)  # For 10:00:00 AM
 
-#Calculate the time to wait in seconds until the purchase time
+# Calculate the time to wait in seconds until the purchase time
 wait_time = (purchase_time - datetime.now()).total_seconds()
 
-#Wait until the specified purchase time
+# Wait until the specified purchase time
 if wait_time > 0:
     print(f"Waiting for {wait_time} seconds until 10:00:00 AM...")
     time.sleep(wait_time)
 
-#open browser(Chrome / Brave)
-driver = webdriver.Brave(executable_path='"C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe"')
+# Configure Chrome options
+chrome_options = Options()
 
- #Navigate to the checkout page URL
+# Disable the "chrome is being controlled by automated test software" message
+chrome_options.add_argument("disable-infobars")
+
+# Open the browser (Chrome)
+driver = webdriver.Chrome(options=chrome_options)
+
+# Navigate to the StubHub checkout URL
 driver.get(checkout_url)
 
+# Close the browser
+driver.quit()
+
+#Navigate to the checkout page URL
+
 #Generic editable part that IDK how to do, the actual checkout and buying process
+
 #use Selenium to interact with elements on the page, fill out forms, click buttons, etc.
- #Below is where I would fill that out.
+
 
 #Example idk:
 #driver.find_element_by_id('input_field_id').send_keys('your_data')
@@ -36,3 +51,4 @@ driver.get(checkout_url)
 
 #To close the browser when you're done:
 #driver.quit() 
+
